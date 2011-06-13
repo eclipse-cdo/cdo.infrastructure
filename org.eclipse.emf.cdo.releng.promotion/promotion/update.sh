ecf71a4d5b149d3df19bc2912b60cd53efca5283
Eike Stepper <stepper@esc-net.de>
2011-06-13 05:58:07 +0000

    Share project "org.eclipse.emf.cdo.releng.promotion" into "https://dev.eclipse.org/svnroot/modeling/org.eclipse.emf.cdo"

---
+++ org.eclipse.emf.cdo.releng.promotion/promotion/update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Initially on build.eclipse.org:
+#svn checkout https://dev.eclipse.org/svnroot/modeling/org.eclipse.emf.cdo/trunk/releng/org.eclipse.emf.cdo.releng/promotion
+
+svn update
+chmod -R u+x *.sh
