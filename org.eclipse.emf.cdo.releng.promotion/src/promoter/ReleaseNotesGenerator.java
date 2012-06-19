97e6465b2c029b0e1fb95626c5131431ae3d8e46
Eike Stepper <stepper@esc-net.de>
2012-06-19 12:59:18 +0200

    Render relnotes to HTML

--- org.eclipse.emf.cdo.releng.promotion/src/promoter/ReleaseNotesGenerator.java
+++ org.eclipse.emf.cdo.releng.promotion/src/promoter/ReleaseNotesGenerator.java
@@ -476,7 +476,7 @@ public class ReleaseNotesGenerator extends PromoterComponent
 
         out.print("<img src=\"../../images/" + severity + ".gif\" alt=\"" + severity + "\">&nbsp;");
         out.print("[<a href=\"" + url + "\">" + issue.getID() + "</a>]&nbsp;" + title);
-        out.print("&nbsp; [v" + issue.getVersion() + "]");
+        out.print("&nbsp;&nbsp;<font color=\"#222222\"><i>v" + issue.getVersion() + "</i></font>");
         out.println("<br/>");
       }
     }
