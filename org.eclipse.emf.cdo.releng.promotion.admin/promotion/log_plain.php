6dfb8d18fecb7a1e786000cd5bec417ec0cd55c1
Eike Stepper <stepper@esc-net.de>
2012-06-22 08:50:56 +0200

    [383171] [Releng] Add task for drop duplication in promotion admin https://bugs.eclipse.org/bugs/show_bug.cgi?id=383171

--- org.eclipse.emf.cdo.releng.promotion.admin/promotion/log_plain.php
+++ org.eclipse.emf.cdo.releng.promotion.admin/promotion/log_plain.php
@@ -8,6 +8,12 @@ echo "<h3>Log Size: $logsize Bytes</h3>\n\n";
 echo "<tt>\n\n";
 
 $content = file_get_contents($logfile);
+
+while (substr($content, 0, 1) == "\n")
+{
+	$content = substr($content, 1);
+}
+
 $content = str_replace("\n", '<br>', $content);
 echo $content;
 
