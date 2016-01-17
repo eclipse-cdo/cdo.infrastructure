9203c0b720991462c9dc7c5eb36d798190ad570c
Eike Stepper <stepper@esc-net.de>
2016-01-17 13:53:06 +0100

    Fix promotion/update.sh

--- org.eclipse.emf.cdo.releng.promotion/promotion/update.sh
+++ org.eclipse.emf.cdo.releng.promotion/promotion/update.sh
@@ -6,7 +6,7 @@ rm -rf cdo.infrastructure
 git clone file:///gitroot/cdo/cdo.infrastructure.git
 
 rm -rf promotion
-cp -a cdo.infrastructure/org.eclipse.emf.cdo.releng.promotion/promotion promotion
+cp -a cdo.infrastructure/org.eclipse.emf.cdo.releng.promotion/promotion ~
 
 chmod u+x promotion/*.sh promotion/promoter/*.sh
 
