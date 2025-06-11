74a7eb093c137e3c6f71abf97c7ec4997088cebf
Eike Stepper <stepper@esc-net.de>
2025-06-11 08:13:39 +0200

    Generate documentation page

--- org.eclipse.emf.cdo.releng.promotion/src/promoter/CDOWebGenerator.java
+++ org.eclipse.emf.cdo.releng.promotion/src/promoter/CDOWebGenerator.java
@@ -23,11 +23,17 @@ import promoter.util.IO.PrintHandler;
  */
 public class CDOWebGenerator extends WebGenerator
 {
-  private static final String TEMPLATE = "https://raw.githubusercontent.com/eclipse-cdo/cdo.www/refs/heads/master/downloads/index.html";
+  private static final String GITHUB = "https://raw.githubusercontent.com/eclipse-cdo/cdo.www/refs/heads/master";
 
-  private static final Pattern BREADCRUMB = pattern("BREADCRUMB");
+  private static final String DOWNLOADS_TEMPLATE = GITHUB + "/downloads/index.html";
 
-  private static final Pattern GENERATED_BODY = pattern("GENERATED_BODY");
+  private static final String DOCUMENTATION_TEMPLATE = GITHUB + "/documentation/index.html";
+
+  private static final Pattern OVERVIEW_PATTERN = Pattern.compile("</table>(.+)<p align=\"right\">", Pattern.DOTALL);
+
+  private static final Pattern BREADCRUMB_PATTERN = pattern("BREADCRUMB");
+
+  private static final Pattern GENERATED_BODY_PATTERN = pattern("GENERATED_BODY");
 
   public CDOWebGenerator()
   {
@@ -37,11 +43,16 @@ public class CDOWebGenerator extends WebGenerator
   protected void generateWeb(WebNode root, PrintStream out) throws IOException
   {
     // All downloads (index.html)
-    generateTemplate(out, TEMPLATE, "<a href=\"downloads.html\">Downloads</a><span>All</span>", IO.print(ps -> super.generateWeb(root, ps)));
+    generateTemplate(out, DOWNLOADS_TEMPLATE, "<a href=\"downloads.html\">Downloads</a><span>All</span>", IO.print(ps -> super.generateWeb(root, ps)));
 
     // Simple downloads (downloads.html)
     printFile("downloads.html", html -> {
-      generateTemplate(html, TEMPLATE, "<span>Downloads</span>", IO.print(ps -> generateDownloads(root, ps)));
+      generateTemplate(html, DOWNLOADS_TEMPLATE, "<span>Downloads</span>", IO.print(ps -> generateDownloads(root, ps)));
+    });
+
+    // Documentation (documentation.html)
+    printFile("documentation.html", html -> {
+      generateTemplate(html, DOCUMENTATION_TEMPLATE, "<span>Documentation</span>", IO.print(ps -> generateDocumentation(root, ps)));
     });
   }
 
@@ -107,11 +118,46 @@ public class CDOWebGenerator extends WebGenerator
     out.println("</div>");
   }
 
+  private void generateDocumentation(WebNode root, PrintStream out)
+  {
+    WebNode releases = root.getChild("releases");
+    BuildInfo buildInfo = releases.getLatestDrop(false);
+    String qualifier = buildInfo.getQualifier();
+    String webLabel = buildInfo.getWebLabel();
+
+    String drop = "https://download.eclipse.org/modeling/emf/cdo/drops/" + qualifier;
+    String help = drop + "/help";
+    String cdodoc = help + "/org.eclipse.emf.cdo.doc/html/";
+    String overview = IO.readURL(cdodoc + "Overview.html");
+
+    Matcher matcher = OVERVIEW_PATTERN.matcher(overview);
+    if (matcher.find())
+    {
+      String body = matcher.group(1).trim();
+
+      // 1) Protect absolute URLs against rewrite in 2) below.
+      body = body.replace("<a href=\"http", "<a HREF=\"http");
+      body = body.replace("<img src=\"http", "<img SRC=\"http");
+
+      // 2) Rewrite relative URLs.
+      body = body.replace("<a href=\"", "<a href=\"" + cdodoc);
+      body = body.replace("<img src=\"", "<img src=\"" + cdodoc);
+
+      String others = "<p class=\"others\"><i>This overview is an extract from the <a href=\"" + help + "\">Help Center</a> of CDO " + webLabel + " (<a href=\""
+          + drop + "\">" + qualifier + "</a>).<br>\n"
+          + "The help centers of other CDO versions are available on the <a href=\"https://download.eclipse.org/modeling/emf/cdo/updates/index.html\">Downloads</a> page.</i></p>\n\n";
+
+      out.println(others);
+      out.println(body);
+      out.println(others);
+    }
+  }
+
   protected static void generateTemplate(PrintStream out, String templateURL, String breadcrumb, String body)
   {
     String template = IO.readURL(templateURL);
-    template = replacePlaceholder(template, BREADCRUMB, breadcrumb);
-    template = replacePlaceholder(template, GENERATED_BODY, body);
+    template = replacePlaceholder(template, BREADCRUMB_PATTERN, breadcrumb);
+    template = replacePlaceholder(template, GENERATED_BODY_PATTERN, body);
     out.print(template);
   }
 
